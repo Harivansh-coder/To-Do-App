6ed0f5c9e39d582a5d428b6fd4477cc531677317
Harivansh Thakur <harivanshthakur8@gmail.com>
2022-06-23 21:52:53 +0530

    adde task model and few assets

--- app/src/main/java/com/harivansh/todo/MainActivity.kt
+++ app/src/main/java/com/harivansh/todo/MainActivity.kt
@@ -1,43 +1,110 @@
 package com.harivansh.todo
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.harivansh.todo.ui.theme.ToDoTheme
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ToDoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+
+                PreviewTaskCard()
             }
         }
     }
 }
 
+data class Task(val taskName: String, val status: Boolean)
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TaskCard(taskName: String) {
+    Card {
+
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(20.dp)
+        ) {
+
+            Text(text = "hello")
+
+            Spacer(modifier = Modifier.width(10.dp))
+            Image(painter = painterResource(id = R.drawable.edit_icon), contentDescription = "edit icon")
+            Spacer(modifier = Modifier.width(10.dp))
+            Image(painter = painterResource(id = R.drawable.delete_icon), contentDescription = "delete icon")
+
+
+        }
+    }
 }
 
-@Preview(showBackground = true)
+
 @Composable
-fun DefaultPreview() {
-    ToDoTheme {
-        Greeting("Android")
+fun Tasks(tasks: List<Task>) {
+    LazyColumn {
+
+        items(tasks) { tasks ->
+
+            TaskCard(taskName = tasks.taskName)
+        }
     }
+
+
+}
+//
+//@Preview(
+//    name = "Light Mode"
+//)
+//@Preview(
+//    uiMode = Configuration.UI_MODE_NIGHT_YES,
+//    showBackground = true,
+//    name = "Dark Mode"
+//)
+
+val tasks = listOf(
+    Task("hello", true),
+    Task("hello", true),
+    Task("hello", true),
+    Task("hello", true),
+    Task("hello", true),
+    Task("hello", true),
+    Task("hello", true),
+    Task("hello", true),
+    Task("hello", true),
+    Task("hello", true),Task("hello", true),
+    Task("hello", true),
+    Task("hello", true),
+    Task("hello", true),
+    Task("hello", true),
+
+)
+
+
+@Preview
+@Composable
+fun PreviewTaskCard() {
+
+    ToDoTheme {
+
+        Tasks(tasks = tasks)
+
+    }
+
 }
---
+++ app/src/main/java/com/harivansh/todo/compnents/TaskCard.kt
@@ -0,0 +1,30 @@
+package com.harivansh.todo.compnents
+
+
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.contentColorFor
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.NonRestartableComposable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+class TaskCard(taskName: String)
+
+//    @Composable
+//    @NonRestartableComposable
+//    fun Card(
+//        modifier: Modifier = Modifier,
+//        shape: Shape = MaterialTheme.shapes.medium,
+//        backgroundColor: Color = MaterialTheme.colors.surface,
+//        contentColor: Color = contentColorFor(backgroundColor),
+//        border: BorderStroke? = null,
+//        elevation: Dp = 1.dp,
+//        content: @Composable () -> Unit
+//    ): Unit{
+//
+//    }
